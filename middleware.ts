42c1a43a751dae4f5b2910c922602eef1cbe3126
Gabriel <gabriel@pop-os.localdomain>
2022-11-09 19:08:01 -0300

    feat: cria logica de autenticacao no front

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "next-auth/react";
+
+export async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith("/manager")) {
+    const signInPage = "/manager/login";
+    const errorPage = "/manager/login";
+
+    if ([signInPage, errorPage].includes(req.nextUrl.pathname)) {
+      return;
+    }
+
+    const requestForNextAuth: any = {
+      headers: {
+        cookie: req.headers.get("cookie"),
+      },
+    };
+
+    const session = await getSession({ req: requestForNextAuth });
+
+    if (session) {
+      return NextResponse.next();
+    }
+
+    const signInUrl = new URL(signInPage, req.nextUrl.origin);
+    return NextResponse.redirect(signInUrl);
+  }
+}
--- pages/manager/index.tsx
+++ pages/manager/index.tsx
@@ -1,5 +1,20 @@
+import { signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+
 export default function Manager() {
+  const { data: session } = useSession()
+
   return (
-    <></>
-  );
+    <>
+      <Head>
+        <title>Manager</title>
+      </Head>
+      <h1>{`Seja bem vindo, ${session?.user.name}`}</h1>
+      <button 
+        onClick={() => signOut()}
+        className="py-2 px-4 bg-purple-500 text-white rounded">
+        Sair
+      </button>
+    </>
+  )
 }
