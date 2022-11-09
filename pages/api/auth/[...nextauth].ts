abd30d4c8161ab60f59086e47d1a39efd17c49b4
Gabriel <gabriel@pop-os.localdomain>
2022-11-09 19:48:46 -0300

    feat: add column is adm

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../src/app/lib/prisma";
+import { prisma } from "../../../src/lib/prisma";
 
 export const authOptions = {
   providers: [
